db63fe93e180b6de7579a2c1b97a9e0918084768
Roman <romandkrv@gmail.com>
2024-11-18 18:06:52 +0300

    add quickSort with comparator

--- src/main/java/ru/derevenskikh/QuickSort.java
+++ src/main/java/ru/derevenskikh/QuickSort.java
@@ -1,5 +1,7 @@
 package ru.derevenskikh;
 
+import java.util.Comparator;
+
 public class QuickSort {
 
     /**
@@ -10,8 +12,51 @@ public class QuickSort {
         quickSort(0, list.size() - 1, list);
     }
 
+    public static void sort(NewArrayList list, Comparator comparator) {
+        quickSort(0, list.size() - 1, list, comparator);
+    }
+
     /**
-     * Realisation of QuickSort
+     * Realisation of QuickSort with Comparator
+     *
+     * @param low        - bound of array
+     * @param high       - bound of array
+     */
+    private static void quickSort(int low, int high, NewArrayList<Object> list, Comparator comparator){
+        Object[] elementData = list.getElementData();
+        if (low >= high) {
+            return;
+        }
+        int middle = low + (high - low) / 2;
+        Object opor = elementData[middle];
+
+        int leftBound = low;
+        int rightBound = high;
+        while (leftBound <= rightBound) {
+            while (comparator.compare(elementData[leftBound], opor) < 0) {
+                leftBound++;
+            }
+            while (comparator.compare(elementData[rightBound], opor) > 0) {
+                rightBound--;
+            }
+            if (leftBound <= rightBound) {
+                Object temp = elementData[leftBound];
+                elementData[leftBound] = elementData[rightBound];
+                elementData[rightBound] = temp;
+                leftBound++;
+                rightBound--;
+            }
+        }
+        if (low < rightBound) {
+            quickSort(low, rightBound, list, comparator);
+        }
+        if (high > leftBound) {
+            quickSort(leftBound, high, list, comparator);
+        }
+    }
+
+    /**
+     * Realisation of QuickSort with Comparable
      *
      * @param low        - bound of array
      * @param high       - bound of array
--- src/test/java/ru/derevenskikh/QuickSortTest.java
+++ src/test/java/ru/derevenskikh/QuickSortTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Comparator;
+
 
 public class QuickSortTest {
     private NewArrayList<Integer> testIntegerList;
@@ -21,19 +23,34 @@ public class QuickSortTest {
         testIntegerList = null;
     }
 
-    @DisplayName("Remove elements from List by index")
+    @DisplayName("Sort list with Comparable)")
     @ParameterizedTest
     @CsvSource(value = {
             "'[1, 2, 4, 8, 11, 56]'; '4,2,8,11,1,56'",
             "'[12, 45, 68]'; '68,45,12'"
     }, delimiter = ';'
     )
-    void sortTest(String expectedToString, String values){
+    void sortComparableTest(String expectedToString, String values){
         for (String value : values.split(",")) {
             testIntegerList.add(Integer.parseInt(value));
         }
         QuickSort.sort(testIntegerList);
 
+        Assertions.assertEquals(expectedToString, testIntegerList.toString());
+    }
+
+    @DisplayName("Sort list with Comparator")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "'[1, 2, 4, 8, 11, 56]'; '4,2,8,11,1,56'",
+            "'[12, 45, 68]'; '68,45,12'"
+    }, delimiter = ';'
+    )
+    void sortComparatorTest(String expectedToString, String values){
+        for (String value : values.split(",")) {
+            testIntegerList.add(Integer.parseInt(value));
+        }
+        QuickSort.sort(testIntegerList, Comparator.naturalOrder());
         Assertions.assertEquals(expectedToString, testIntegerList.toString());
     }
 }
